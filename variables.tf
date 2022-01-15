4116e360b7cd236ad37da08996c5bdef21ed6798
Cedric Kienzler <github@cedric-kienzler.de>
2022-01-16 00:15:49 +0100

    remove cf for now

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "cloudflare_email" {
-  description = ""
-  sensitive   = true
-}
-
-variable "cloudflare_api_key" {
-  description = ""
-  sensitive   = true
-}
+#variable "cloudflare_email" {
+#  description = ""
+#  sensitive   = true
+#}
+#
+#variable "cloudflare_api_key" {
+#  description = ""
+#  sensitive   = true
+#}
 
 variable "hcloud_token" {
   description = ""
