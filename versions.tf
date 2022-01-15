a816c6db13cf54abe1d7b02920c1cac6eedfcf8f
Cedric Kienzler <github@cedric-kienzler.de>
2022-01-16 00:00:43 +0100

    bump tf version

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0.9"
+  required_version = ">= 1.1.3"
   required_providers {
     hcloud = {
       source = "hetznercloud/hcloud"
@@ -10,7 +10,7 @@ terraform {
     }
   }
 
-  backend "remote" {
+  cloud {
     organization = "cedi"
     workspaces {
       name = "elbschloss"
