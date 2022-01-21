618cac2e10a10d60e508915216f3982b987edf29
Cedric Kienzler <github@cedric-kienzler.de>
2022-01-21 14:56:01 +0100

    add cedi@mae

--- sshkeys.tf
+++ sshkeys.tf
@@ -1,3 +1,7 @@
+resource "hcloud_ssh_key" "cedi_mae" {
+  name       = "cedi@mae"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOO9DMiwRjCCWvMA9TKYxRApgQx3g+owxkq9jy1YyjGN cedi@mae"
+}
 resource "hcloud_ssh_key" "cedi_ivy" {
   name       = "cedi@ivy"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKKQwlVWSGICyOiVryEdEp8bR+ltCxSeikxPTRRgSssL"
